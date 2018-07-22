a356e962410bdfa5da75c050a5994a9781164ee0
Mohamed Araby <mohamedaraby1296@gmail.com>
2018-07-22 23:38:29 +0200

    .

---
+++ Assets/Scripts/Kill.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class Kill : MonoBehaviour {
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "Player")
+            Destroy(this.gameObject);
+    }
+}
---
+++ Assets/Scripts/UIButtonLevelLoad.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class UIButtonLevelLoad : MonoBehaviour {
+	
+	
+	public void loadLevel() {
+        //Load the level from LevelToLoad
+        if (SceneManager.GetActiveScene().name == "Level1")
+        {
+            SceneManager.LoadScene("Level1");
+        }
+        else if (SceneManager.GetActiveScene().name=="Level2" )
+        {
+            SceneManager.LoadScene("Level2");
+        }
+    }
+    public void loadLevel1()
+    {
+        //Load the level from LevelToLoad
+       
+            SceneManager.LoadScene("Level1");
+      
+    }
+    public void loadLevel2()
+    {
+        //Load the level from LevelToLoad
+
+        SceneManager.LoadScene("Level2");
+
+    }
+}
